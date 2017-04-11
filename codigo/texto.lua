8fe9490f6dcd003222ab451d2cc07ec771d1984b
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2017-04-11 08:23:09 -0300

    Update texto.lua

--- codigo/texto.lua
+++ codigo/texto.lua
@@ -2,5 +2,5 @@ Livre = "Livre"
 Ocupado = "Ocupado"
 Reservado = "Reser."
 
-Ajuda = "mudar_mesa(numero, estado) \n numero = numero da mesa \n estado = LIVRE/OCUPADO/RESERVADO"
-Estado_nao_reconhecido = "O estado que você escreveu não é nenhum dos seguintes: LIVRE, OCUPADO, RESERVADO"
+Estado_nao_reconhecido = "O estado que voce escreveu nao e nenhum dos seguintes: LIVRE, OCUPADO, RESERVADO"
+Pedir_a_hora_da_reserva = "Diga o horario da reserva"
