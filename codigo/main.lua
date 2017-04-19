d2e7fefce5b8ee0bd6ba6ea075236fac798e6b3f
Thiago Lages de Alencar <thiagola92@gmail.com>
2017-04-19 01:00:42 -0300

    Update

--- codigo/main.lua
+++ codigo/main.lua
@@ -1,3 +1,12 @@
+-- Titulo: main
+-- Autoria: Thiago Lages
+-- Data: 11/04/2017
+-- Versão: 1.01
+-- Conteúdo: ~135 linhas
+
+-- Função que recebe toda a informação de uma mesa como tabela e transforma em uma String.
+-- ANTES: Uma das linhas da tabela mesa.
+-- DEPOIS: Uma string com todas as informações da linha.
 local function info_em_string(mesa_info)
     toda_informacao = ""
     
@@ -8,24 +17,25 @@ local function info_em_string(mesa_info)
     return toda_informacao
 end
 
-local function exibir_mesas()
-    print("----------------------------------------------------------")
-    
-    for mesa_numero, mesa_info in pairs(mesa) do
-        print(mesa_numero .. "\t" .. info_em_string(mesa_info))
-    end
-end
-
+-- Muda o estado da mesa para Livre.
+-- ANTES: Mesa se encontra em um estado X.
+-- DEPOIS: Mesa tem o estado mudado para Livre e o horário dela alterado para "NA".
 local function mesa_livre(numero)
     mesa[numero][1] = Livre
 	mesa[numero][3] = "NA"
 end
 
+-- Muda o estado da mesa para Ocupado.
+-- ANTES: Mesa se encontra em um estado X.
+-- DEPOIS: Mesa tem o estado mudado para Ocupado e o horário é alterado para quando foi ocupado.
 local function mesa_ocupada(numero)
     mesa[numero][1] = Ocupado
 	mesa[numero][3] = os.date("%H" .. ":" .. "%M")
 end
 
+-- Muda o estado da mesa para Reservada.
+-- ANTES: Mesa se encontra em um estado X.
+-- DEPOIS: Mesa tem o estado mudado para Reservada e o horário é alterado para quando foi reservada.
 local function mesa_reservada(numero)
     mesa[numero][1] = Reservado
 	
@@ -35,11 +45,26 @@ local function mesa_reservada(numero)
 	mesa[numero][3] = horario
 end
 
+-- Printa toda a informação das mesas para o usuário.
+-- ANTES: Informações dos usuários estão em uma tabela.
+-- DEPOIS: Informações dos usuários estão sendo exibidas na tela.
+function exibir_mesas()
+    print("----------------------------------------------------------")
+    
+    for mesa_numero, mesa_info in pairs(mesa) do
+        print(mesa_numero .. "\t" .. info_em_string(mesa_info))
+    end
+end
 
+-- Define o estado de uma mesa.
+-- ANTES: Mesa esta em um estado qualquer.
+-- DEPOIS: Mesa vai para um dos seguintes estados: Livre, Ocupada ou Reservada.
 function mesa_estado(numero, estado)
     numero = tonumber(numero)
-
-    if estado == LIVRE then
+    
+    if mesa[numero] == nil then
+		print(Mesa_nao_existe)
+    elseif estado == LIVRE then
 		mesa_livre(numero)
     elseif estado == OCUPADO then
 		mesa_ocupada(numero)
@@ -52,16 +77,37 @@ function mesa_estado(numero, estado)
     exibir_mesas()
 end
 
-function mudar_mesa(numero, estado, lugares, horario)
+-- Altera o número de lugares que uma mesa tem.
+-- ANTES: Mesa possui lugar para X pessoas.
+-- DEPOIS: Mesa possui lugar para Y pessoas.
+function mesa_lugares(numero, lugares)
     numero = tonumber(numero)
 
-    mesa[numero][1] = estado
-    mesa[numero][2] = lugares
-    mesa[numero][3] = horario
+    if mesa[numero] == nil then
+        mesa[numero][2] = lugares
+    end
     
     exibir_mesas()
 end
 
+-- Altera o horário de uma mesa se ela não estiver Livre.
+-- ANTES: Mesa possui horario X.
+-- DEPOIS: Mesa possui horario Y.
+function mesa_horario(numero, horario)
+    numero = tonumber(numero)
+    
+    if mesa[numero] == nil then
+		print(Mesa_nao_existe)
+    elseif (mesa[numero][1] ~= LIVRE) then
+        mesa[numero][3] = horario
+    end
+    
+    exibir_mesas()
+end
+
+-- Adiciona uma nova mesa ao sistema.
+-- ANTES: Tem X mesas no sistema.
+-- DEPOIS: Tem X+1 mesas no sistema.
 function adicionar_mesa(numero, estado, lugares, horario)
     numero = tonumber(numero)
 
@@ -69,3 +115,20 @@ function adicionar_mesa(numero, estado, lugares, horario)
 
     exibir_mesas()
 end
+
+-- Remove uma mesa do sistema.
+-- ANTES: Mesa X esta no sistema.
+-- DEPOIS: Mesa X não esta mais no sistema.
+function remover_mesa(numero)
+    numero = tonumber(numero)
+
+    if mesa[numero] == nil then
+		print(Mesa_nao_existe)
+    else
+        mesa[numero] = nil
+    end
+
+    exibir_mesas()
+end
+
+
--- codigo/textos.lua
+++ codigo/textos.lua
@@ -3,4 +3,5 @@ Ocupado = "Ocupado"
 Reservado = "Reser."
 
 Estado_nao_reconhecido = "O estado que voce escreveu nao e nenhum dos seguintes: LIVRE, OCUPADO, RESERVADO"
+Mesa_nao_existe = "A mesa que voce tentou acessar nao existe"
 Pedir_a_hora_da_reserva = "Diga o horario da reserva"
